5c155e5a6fa9c9e67335712c429a8021cd8221ce
leitolima <leo-lima-08@hotmail.com>
2021-02-05 20:33:50 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/login';
 import SignUp from './pages/signup';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <ApolloProvider client={client}>
             <Router>
                 <Switch>
+                    <Route exact path="/" component={Home}/>
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/signup" component={SignUp}/>
                 </Switch>
