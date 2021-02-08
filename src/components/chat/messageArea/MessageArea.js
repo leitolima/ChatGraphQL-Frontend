f6c696d0e32a4c4072a8ea503700ddfe21b61a6f
leitolima <leo-lima-08@hotmail.com>
2021-02-08 12:36:26 -0300

    useQuery instead of useLazyQuery

--- src/components/chat/messageArea/MessageArea.js
+++ src/components/chat/messageArea/MessageArea.js
@@ -4,7 +4,7 @@ import UserContext from '../../../context/userContext';
 import Messages from './Messages';
 import InputArea from './InputArea';
 //GraphQL
-import { useLazyQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { GET_MESSAGES } from '../../../graphql/querys';
 
 const MessageArea = () => {
@@ -12,19 +12,11 @@ const MessageArea = () => {
     const context = useContext(UserContext);
     const { channel } = context;
 
-    const [toDo, { data }] = useLazyQuery(GET_MESSAGES, {
+    const { data } = useQuery(GET_MESSAGES, {
         variables: {
             id: channel.id
         }
     });
-
-    useEffect(() => {
-        fetchMessages();
-    }, [channel]);
-
-    const fetchMessages = async () => {
-        toDo();
-    }
 
     return (
         <>
